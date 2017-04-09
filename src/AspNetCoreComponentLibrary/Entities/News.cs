c32b612bb429fa6b70eb6f830a345ed6da34304b
Andrey Murin <96467840@mail.ru>
2017-04-09 12:13:39 +0300

    новости и setup

---
+++ src/AspNetCoreComponentLibrary/Entities/News.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreComponentLibrary.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreComponentLibrary
+{
+    public class News : BaseDM<long>, IBlockable
+    {
+        public long SiteId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public DateTime? Date { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool InWww { get; set; }
+        public bool InVk { get; set; }
+        public bool InFb { get; set; }
+        public string Page { get; set; }
+        public string Image { get; set; }
+        public string Search { get; set; }
+        public string SeoTitle { get; set; }
+        public string SeoKeywords { get; set; }
+        public string SeoDescription { get; set; }
+        public string ExternalId { get; set; }
+    }
+}
---
+++ src/AspNetCoreComponentLibrary/Models/SetupModels.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreComponentLibrary
+{
+    public class SetupIM
+    {
+        public string Site { get; set; }
+        public string Host { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public IActionResult ToActionResult(Controller controller)
+        {
+            var vm = new SetupVM() { Input = this };
+            if (controller.Request.Method == "post")
+            {
+
+            }
+            return controller.View();
+        }
+    }
+
+    public class SetupVM
+    {
+        public SetupIM Input { get; set; }
+        public Exception Error { get; set; }
+
+    }
+}

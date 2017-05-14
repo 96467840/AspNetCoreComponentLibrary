908099b9efafd67efe285265b8e43ea4ce103fd0
Andrey Murin <96467840@mail.ru>
2017-05-14 20:56:42 +0300

    перенес функцию в свой хелпер

--- src/AspNetCoreComponentLibrary/Helpers/UrlHelperExtensions.cs
+++ src/AspNetCoreComponentLibrary/Helpers/UrlHelperExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -56,5 +57,43 @@ namespace AspNetCoreComponentLibrary
             Dictionary<string, object> rv = null;
             return url.RouteUrlWithCulture(routeName, rv);
         }
+
+        public static string CurrentUrl(this IUrlHelper Url, object replaces = null)
+        {
+            var rd = Url.ActionContext.RouteData;
+            var routeName = rd?.GetRouteName();
+            var currentRV = rd?.Values?.ToDictionary(i => i.Key, i => i.Value);
+            if (currentRV == null) currentRV = new Dictionary<string, object>();
+            string path = null;
+            foreach (var r in currentRV)
+            {
+                if (r.Key == "path")
+                {
+                    currentRV["path"] = "[___path___]";
+                    path = r.Value as string;
+                    break;
+                }
+            }
+            foreach (var q in Url.ActionContext.HttpContext.Request.Query)
+            {
+                currentRV[q.Key] = q.Value;
+            }
+            if (replaces != null)
+            {
+                foreach (var r in replaces.AsDictionary())
+                {
+                    currentRV[r.Key] = r.Value;
+                }
+            }
+
+            var res = Url.RouteUrlWithCulture(routeName, currentRV);
+            // решение ппц какое стремное, но пока не найду красивого и универсального решения будет так (по другому нельзя сделать path="faq/index")
+            if (path != null)
+            {
+                res = res.Replace("%5b___path___%5d", path);
+            }
+            return res;
+        }
+
     }
 }
--- src/AspNetCoreComponentLibrary/Infrastructure/Utils.cs
+++ src/AspNetCoreComponentLibrary/Infrastructure/Utils.cs
@@ -33,43 +33,6 @@ namespace AspNetCoreComponentLibrary
             return EqualityComparer<K>.Default.Equals(item, default(K));
         }
 
-        public static string CurrentUrl(this IUrlHelper Url, object replaces = null)
-        {
-            var rd = Url.ActionContext.RouteData;
-            var routeName = rd?.GetRouteName();
-            var currentRV = rd?.Values?.ToDictionary(i => i.Key, i => i.Value);
-            if (currentRV == null) currentRV = new Dictionary<string, object>();
-            string path = null;
-            foreach (var r in currentRV)
-            {
-                if (r.Key == "path")
-                {
-                    currentRV["path"] = "[___path___]";
-                    path = r.Value as string;
-                    break;
-                }
-            }
-            foreach (var q in Url.ActionContext.HttpContext.Request.Query)
-            {
-                currentRV[q.Key] = q.Value;
-            }
-            if (replaces != null)
-            {
-                foreach (var r in replaces.AsDictionary())
-                {
-                    currentRV[r.Key] = r.Value;
-                }
-            }
-
-            var res = Url.RouteUrlWithCulture(routeName, currentRV);
-            // решение ппц какое стремное, но пока не найду красивого и универсального решения будет так (по другому нельзя сделать path="faq/index")
-            if (path != null)
-            {
-                res = res.Replace("%5b___path___%5d", path);
-            }
-            return res;
-        }
-
         public static bool EqualsIC(this string arg1, string arg2) {
             if (arg1 == null)
                 throw new ArgumentNullException(nameof(arg1));
